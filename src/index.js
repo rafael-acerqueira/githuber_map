9a95a47a9268841ea261ad5e642d39f77405b552
Rafael Aquino <rafael.acerqueirah@gmail.com>
2018-01-06 10:48:45 -0300

    Add and install react-native-modal

--- src/index.js
+++ src/index.js
@@ -1,25 +1,56 @@
 /* Core */
 import React, { Component } from 'react';
 
+import { View, Text, TouchableOpacity } from 'react-native';
+
 import MapView from 'react-native-maps';
+import Modal from 'react-native-modal';
 
 import './config/ReactotronConfig';
 
 import styles from './styles';
 
 export default class App extends Component {
+  state = {
+    isModalVisible: false,
+  }
+
+  toggleModal = () => this.setState({ isModalVisible: !this.state.isModalVisible });
+
   render() {
     return (
-      <MapView
-        style={styles.map}
-        region={{
-          latitude: -27.2177659,
-          longitude: -49.6451598,
-          latitudeDelta: 0.0042,
-          longitudeDelta: 0.0031,
-        }}
-      >
-      </MapView>
+      <View style={styles.container}>
+        <MapView onLongPress={(event) => {this.toggleModal()}}
+          style={styles.map}
+          region={{
+            latitude: -27.2177659,
+            longitude: -49.6451598,
+            latitudeDelta: 0.0042,
+            longitudeDelta: 0.0031,
+          }}
+        >
+        </MapView>
+        <Modal
+          style={styles.modal}
+          isVisible={this.state.isModalVisible}
+          backdropOpacity={0.6}
+        >
+          <View>
+            <Text style={styles.modalTitle}>Adicionar novo local</Text>
+          </View>
+          <View style={styles.modalButtonsContainer}>
+            <TouchableOpacity
+              style={styles.modalButton}
+              onPress={() => this.setState({ isModalVisible: false })}
+            >
+              <Text>Cancelar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.modalButton} >
+              <Text>Salvar</Text>
+            </TouchableOpacity>
+          </View>
+        </Modal>
+      </View>
     );
   }
 }
--- src/styles.js
+++ src/styles.js
@@ -1,8 +1,31 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+  },
   map: {
     ...StyleSheet.absoluteFillObject,
+  },
+  modal: {
+    backgroundColor: '#FFF',
+    margin: 20,
+    padding:20,
+    borderRadius: 5
+  },
+  modalTitle: {
+    color: '#333',
+    fontSize: 18,
+    textAlign: 'center',
+  },
+  modalButtonsContainer: {
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    flexDirection: 'row',
+  },
+  modalButton: {
+    height: 42,
+    borderRadius: 5
   },
 });
 
